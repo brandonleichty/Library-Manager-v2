f6e4754e856bf9fb609850522bb2db86062ac645
Brandon Leichty <brandonl@me.com>
2018-05-02 17:39:43 -0500

    updated route name for check out books

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
-const Books = require('../models').Book;
+const Book = require('../models').Book;
+const Loan = require('../models').Loan;
+const Patron = require('../models').Patron;
 
-// route: /books
+// get all books 
 router.get('/', (req, res) => {
-  Books.findAndCountAll()
-  .then((results) => {
-    res.render('books', { books: results, pageTitle: 'Books',
+  Book.findAndCountAll()
+    .then((bookResults) => {
+      res.render('books', {
+        books: bookResults.rows,
+        pageTitle: 'Books',
       });
-  });
+    });
 });
 
-module.exports = router;
+
+
+// Display checked out books: 
+
+router.get('/checked_out', (req, res, next) => {
+
+      Book.findAll({
+        include: [{
+          model: Loan,
+          where: {
+            loaned_on: {
+              [Sequelize.Op.ne]: null
+            },
+            returned_on: {
+              [Sequelize.Op.eq]: null
+            }
+          }
+        }]
+      }).then((results) => {
+        res.render('books', {
+          books: results,
+          pageTitle: 'Checked out books',
+        });
+      });
+    });
+
+      module.exports = router;
