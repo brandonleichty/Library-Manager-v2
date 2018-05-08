dc9802e0a946669ee1109b543d59d946cb9d025d
Brandon Leichty <brandonl@me.com>
2018-05-07 22:38:02 -0500

    all routes are working. Need to refactor

---
+++ routes/loans.js
@@ -0,0 +1,160 @@
+const express = require('express');
+const router = express.Router();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const moment = require('moment');
+
+const Book = require('../models').Book;
+const Loan = require('../models').Loan;
+const Patron = require('../models').Patron;
+
+
+
+
+router.get('/', function (req, res, next) {
+
+  Loan.findAll({
+      include: [Book, Patron]
+    })
+    .then(function (results) {
+      res.render('all_loans', {
+        loans: results,
+        pageTitle: 'Loans'
+      });
+    });
+});
+
+
+
+/** GET return book form page. */
+router.get('/return/:id', async (req, res) => {
+  const loan = await Loan.findById(req.params.id);
+  const [book, patron] = await Promise.all([Book.findById(loan.book_id), Patron.findById(loan.patron_id)]);
+  if (loan) {
+    res.render('return_book', {
+      loan,
+      patron,
+      book,
+      title: 'Return book',
+      today: moment().format('YYYY-MM-DD')
+    });
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+
+
+
+router.post('/return/:id', (req, res, next) => {
+
+  Loan.findById(req.params.id)
+    .then((loan) => { // update loan record
+      return loan.update(req.body);
+    })
+    .then((loan) => { // redirect to loan listing page
+      res.redirect('/books');
+    })
+    .catch((err) => { // handle any errors
+      if (err.name === 'SequelizeValidationError') {
+        renderReturnLoan(req, res, err);
+      } else res.sendStatus(500);
+    });
+});
+
+
+// Get overdue loans
+
+router.get('/overdue_loans', function (req, res, next) {
+  Loan.findAll({
+      include: [Book, Patron],
+      where: {
+        return_by: {
+          [Op.lt]: new Date()
+        },
+        returned_on: null
+      }
+    })
+    .then(function (loans) {
+      res.render('all_loans', {
+        loans,
+        pageTitle: 'Overdue Loans',
+      });
+    });
+});
+
+
+// Get checked out loans
+router.get('/checked_loans', (req, res) => {
+  Loan.findAll({
+      include: [Book, Patron],
+      where: {
+        returned_on: {
+          [Op.eq]: null
+        }
+      }
+    })
+    .then(function (loans) {
+      res.render('all_loans', {
+        loans,
+        pageTitle: 'Checked Loans',
+      });
+    });
+});
+
+
+
+// New loan
+
+router.get('/new_loan', (req, res) => {
+
+  const loan = Loan.build({
+    loaned_on: moment().format('YYYY-MM-DD'),
+    return_by: moment().add(7, 'days').format('YYYY-MM-DD')
+  });
+
+  Book.findAll()
+    .then((books) => {
+      Patron.findAll() // get all the patrons
+        .then((patrons) => {
+          res.render('new_loan', { // render the new loan form
+            loan: Loan.build({
+              loaned_on: moment().format('YYYY-MM-DD'),
+              return_by: moment().add(7, 'days').format('YYYY-MM-DD')
+            }),
+            books,
+            patrons,
+            pageTitle: 'New Loan'
+          });
+        });
+    });
+});
+
+// Save new loan
+router.post('/new_loan', (req, res) => {
+
+  Loan.create(req.body) // create a new loan and redirect
+    .then((loan) => { // to loans index page
+      res.redirect('/loans');
+    })
+    .catch((err) => { // or show error messages
+      if (err.name === 'SequelizeValidationError') {
+        Book.findAll() // get all the books
+          .then((books) => {
+            Patron.findAll() // get all the patrons
+              .then((patrons) => {
+                res.render('new_loan', { // render the new loan form
+                  loan: Loan.build(req.body),
+                  books,
+                  patrons,
+                  errors: err.errors,
+                  pageTitle: 'New Loan'
+                });
+              });
+          });
+      }
+    });
+});
+
+
+module.exports = router;
---
+++ routes/patrons.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const router = express.Router();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+const Book = require('../models').Book;
+const Loan = require('../models').Loan;
+const Patron = require('../models').Patron;
+
+
+
+
+
+
+
+router.get('/', (req, res, next) => {
+
+  Patron.findAndCountAll()
+    .then((results) => {
+      res.render('all_patrons', {
+        patrons: results.rows,
+        pageTitle: 'Patrons'
+      });
+    });
+});
+
+// New Patron 
+router.get('/new_patron', (req, res) => {
+  try {
+    res.render('new_patron', {
+      patron: Patron.build(),
+      pageTitle: 'New Patron'
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// Create a new Patron
+router.post('/new_patron', (req, res) => {
+  Patron.create(req.body)
+    .then(patron => {
+      res.redirect('/patrons');
+    })
+    .catch(err => {
+      if (err.name === 'SequelizeValidationError') {
+        res.render('new_patron', {
+          patron: Patron.build(req.body),
+          pageTitle: 'New Patron',
+          errors: err.errors
+        });
+      } else {
+        throw err;
+      }
+    });
+});
+
+
+// Get patron details
+
+router.get('/details/:id', (req, res, next) => {
+
+  Patron.findById(req.params.id)
+    .then((patron) => {
+      Loan.findAll({
+          include: [Book, Patron],
+          where: {
+            patron_id: patron.id
+          }
+        })
+        .then((loans) => {
+          res.render('patron_details', {
+            patron,
+            loans,
+          });
+        });
+    });
+});
+
+
+// Edit Patron details 
+
+router.post('/details/:id', (req, res) => {
+
+  Patron.findById(req.params.id)
+    .then((patron) => { // update patron record
+      return patron.update(req.body)
+    })
+    .then((patron) => { // redirect to book listing page
+      res.redirect('/patrons')
+    })
+    .catch((err) => { // handle validation errors
+      if (err.name === 'SequelizeValidationError') {
+        //   res.render('patron_details', {
+        //   patron: Patron.build(req.body),
+        //   pageTitle: 'Patron Details',
+        //   errors: err.errors,
+        //   loans
+        // });
+        Patron.findById(req.params.id)
+          .then((patron) => {
+            Loan.findAll({
+                include: [Book, Patron],
+                where: {
+                  patron_id: patron.id
+                }
+              })
+              .then((loans) => {
+                res.render('patron_details', {
+                  patron: Patron.build(req.body),
+                  loans,
+                  errors: err.errors,
+                });
+              });
+          });
+      } else res.send(500);
+    });
+});
+
+module.exports = router;
