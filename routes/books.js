2f5847bca2da56f006cf688fda5c7f55c66e097c
Brandon Leichty <brandonl@me.com>
2018-05-07 22:37:33 -0500

    have working routes for books. Need to refactor

--- routes/books.js
+++ routes/books.js
@@ -24,60 +24,156 @@ router.get('/', (req, res) => {
 
 router.get('/checked_out', (req, res, next) => {
 
-      Book.findAll({
-        include: [{
-          model: Loan,
-          where: {
-            loaned_on: {
-              [Sequelize.Op.ne]: null
-            },
-            returned_on: {
-              [Sequelize.Op.eq]: null
-            }
-          }
-        }]
-      }).then((results) => {
-        res.render('books', {
-          books: results,
-          pageTitle: 'Checked out books',
-        });
-      });
-    });
-
-
-
-    // overdue books
-    router.get('/overdue_books', (req, res, next) => {
-
-      Book.findAll({
-        include: [
-          {
-            model: Loan,
-            where: {
-              return_by: {
-                [Sequelize.Op.lt]: new Date()
-              },
-              returned_on: null
-            }
+  Book.findAll({
+    include: [{
+      model: Loan,
+      where: {
+        loaned_on: {
+          [Op.ne]: null
+        },
+        returned_on: {
+          [Op.eq]: null
+        }
+      }
     }]
-      }).then((results) => {
-        res.render('books', {
-          books: results,
-          pageTitle: 'Overdue out books',
+  }).then((results) => {
+    res.render('books', {
+      books: results,
+      pageTitle: 'Checked out books',
+    });
+  });
+});
+
+
+
+// overdue books
+router.get('/overdue_books', (req, res, next) => {
+
+  Book.findAll({
+    include: [{
+      model: Loan,
+      where: {
+        return_by: {
+          [Sequelize.Op.lt]: new Date()
+        },
+        returned_on: null
+      }
+    }]
+  }).then((results) => {
+    res.render('books', {
+      books: results,
+      pageTitle: 'Overdue out books',
+    });
+  });
+});
+
+
+// new book GET route
+
+router.get('/new_book', (req, res) => {
+  console.log('💩💩💩💩💩💩')
+  res.render('new_book', {
+    book: Book.build(),
+    pageTitle: 'New book'
+  });
+});
+
+
+// new book POST route
+
+router.post('/new_book', (req, res) => {
+  Book.create(req.body)
+    .then(book => {
+      res.redirect('/books');
+    })
+    .catch(err => {
+      if (err.name === 'SequelizeValidationError') {
+        res.render('new_book', {
+          book: Book.build(req.body),
+          title: 'New book',
+          errors: err.errors
         });
+      } else {
+        throw err;
+      }
+    })
+    .catch(err => {
+      res.sendStatus(500);
+    });
+});
+
+
+/** GET book detail page */
+router.get('/details/:id', function (req, res, next) {
+  Book.findById(req.params.id, {
+    include: [{
+      model: Loan,
+      include: [{
+        model: Patron
+      }]
+    }]
+  }).then(function (results) {
+    if (results) {
+      res.render('book_details', {
+        book: results,
+        title: results.title
       });
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+
+
+
+
+
+
+
+/**
+ * POST save book details
+ * /books/details/:id
+ */
+
+router.post('/details/:id', (req, res, next) => {
+
+  Book.findById(req.params.id)
+    .then((book) => { // update book record
+      return book.update(req.body)
+    })
+    .then((book) => { // redirect to book listing page
+      res.redirect('/books')
+    })
+    .catch((err) => { // handle validation errors
+      if (err.name === 'SequelizeValidationError') {
+        let book = Book.build(req.body);
+        renderUpdateBookDetails(res, book, err);
+      } else res.send(500);
     });
-
-
-    // new book
-
-    router.get('/new_book', (req, res) => {
-      console.log('💩💩💩💩💩💩')
-      res.render('new_book', { pageTitle: 'New book' });
-    });
-
-      module.exports = router;
+});
 
 
 
+/** PUT return book. */
+router.put('/details/:id/return', function (req, res, next) {
 
+  console.log('YAYYYY');
+
+  Loan.update({
+    returned_on: req.body.returned_on,
+  }, {
+    where: {
+      book_id: req.params.id
+    }
+  }).then((results) => {
+    res.redirect('/books');
+  }).catch((err) => {
+    if (err.name === 'SequelizeValidationError') {
+      console.log('poop');
+    }
+  });
+});
+
+
+module.exports = router;
